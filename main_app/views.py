75815389f532ead5cf6dc6559f68de8bbf807f0b
Igor Sokolov <wasp100wasp100@gmail.com>
2020-09-26 18:01:08 +0300

    Create navigation bar

--- main_app/views.py
+++ main_app/views.py
@@ -9,35 +9,15 @@ def index():
     return render_template(
         "index.html",
         title='Home',
+        nav_item_id='home',
         # mycontent="Welcome to Awesome School!",
     )
 
 
-@app.route('/auth/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
-            error = 'User {} is already registered.'.format(username)
-
-        if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
-
-        flash(error)
-
-    return render_template('register.html')
+@app.route('/progress')
+def account():
+    return render_template(
+        'progress.html',
+        title='Progress',
+        nav_item_id='progress',
+    )
